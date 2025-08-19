92de8cf255673317257b096c13543706e96d581a
Bala27032002 <bmb4600@gmail.com>
2025-08-19 13:41:01 +0530

    Initial commit

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'bmb4600@gmail.com',
+    phone: '+91 9443844825',
+    address: 'Thoothukudi - 628 704, TN',
+
+    sheetAPI: ''
+}
---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/BalaMuthu_Resume.pdf'
+import Profile_img from '../assets/svg/Profile_image.jpeg'
+export const headerData = {
+    name: 'Bala Muthu Manikandan M',
+    title: "Web Developer | MERN Stack",
+    desciption:"I am a passionate MERN Stack Developer with 2 years of experience building scalable web applications and user-friendly interfaces. Skilled in React.js, Node.js, Express.js, and MongoDB, I enjoy solving problems, writing clean code, and delivering impactful solutions.",
+    image: Profile_img,
+    resumePdf: resume
+}
